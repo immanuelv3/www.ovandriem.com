cdf9f90901ada28145bcbe3892968375f0e01804
immanuelv3 <I.r.vandriem@hotmail.com>
2025-08-31 08:05:14 +0200

    Add files via upload

--- scriipt.js
+++ scriipt.js
@@ -50,7 +50,7 @@ document.writeln('</div>');
 document.writeln('<div class="dropdown">');
 document.writeln('<button class="dropbtn" onclick="location.href=\'/music.html\'">music</button>');
 document.writeln('<div class="dropdown-content">');
-document.writeln('<a href="">musical theory</a>');
+document.writeln('<a href="/music/tact.html">musical theory</a>');
 document.writeln('</div>');
 document.writeln('</div>');
 
