611f2dfbe0095263a19ac37f5d7f794ef0e29340
immanuelv3 <immanuelv3@users.noreply.github.com>
2025-09-03 17:30:25 +0200

    sa

---
+++ sibomenu.js
@@ -0,0 +1,7 @@
+// Music dropdown
+document.writeln('<div class="dropdown">');
+document.writeln('<button class="dropbtn" onclick="location.href=\'/music.html\'">music</button>');
+document.writeln('<div class="dropdown-content">');
+document.writeln('<a href="/music/tact.html">musical theory</a>');
+document.writeln('</div>');
+document.writeln('</div>');
